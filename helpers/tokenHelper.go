d155ada3eac53496dfaf02a1fbf4858ce3596177
Micah <micahshallom@gmail.com>
2023-10-31 04:23:53 +0100

    added authenticate middleware functionality

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,13 +1,17 @@
 package helpers
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
-
+	"fmt"
 	"github.com/Micah-Shallom/modules/database"
 	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -49,4 +53,63 @@ func GenerateAllTokens(email string, firstName string, lastName string, userType
 		return
 	}
 	return token, refreshToken, err
+}
+
+func ValidateToken(signedToken string)(claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(token *jwt.Token)(interface {}, error){
+			return []byte(SECRET_KEY), nil
+		},
+	)
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = fmt.Sprintf("the token is invalid")
+		msg = err.Error()
+		return
+	}
+	if claims.ExpiresAt < time.Now().Local().Unix(){
+		msg = fmt.Sprintf("Token is expired")
+		msg = err.Error()
+		return
+	}
+	return claims, msg
+}
+
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userid string) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.E{"token", signedToken})
+	updateObj = append(updateObj, bson.E{"refresh_token", signedRefreshToken})
+
+	Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{"updatedAt", Updated_at})
+
+	upsert := true
+	filter := bson.M{"userid": userid}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	_, err := userCollection.UpdateOne(
+		ctx,
+		filter,
+		bson.D{
+			{"$set", updateObj},
+		},
+		&opt,
+	)
+	defer cancel()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	// return
 }
